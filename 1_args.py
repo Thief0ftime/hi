c255fea55a5115dfbc852eaea5a14c55eaff3d5d
Thief0ftime <sp1kexx89@gmail.com>
2021-10-08 23:06:52 +0300

    1task

---
+++ 1_args.py
@@ -0,0 +1,7 @@
+import sys
+#name=sys.argv[1:]
+#n=str(name)
+#print('Hello,'+n)
+#print(name)
+name=str(sys.argv[1])
+print('Hello, '+name+'!!!')
--- hello1.py
+++ hello1.py
@@ -1,15 +1,22 @@
-exit=6*60+52
-low=8+15/60
-med=7+12/60
-calc=exit+1*low+3*med+1*low
-x=calc/60
-x1=int(x)
-x2=x-x1
-ques=x2*0.6
-sx=str(x1)
-sq=int(ques*100)
-sqr=str(sq)
-print(sx+(':')+sqr)
+exit=6*60**2+52*60
+low=8*60+15
+med=7*60+12
+calc=exit+1*low+3*med+1*low #Here we got 27006
+h=calc/60**2  #here we start calculate hour 
+h1=int(h)    
+qh=str(h1)     #here we end
+m=(h-h1)*60
+m1=int(m)
+qm=str(m1)
+s=(m-m1)*60
+s1=int(s)
+qs=str(s1)
+#ques=x2*0.6
+#sh=str(h1)
+#sq=int(ques*100)
+#sqr=str(sq)
+#print(sh+(':')+sqr)
+print(qh+':'+qm+':'+qs)
 
 
 
